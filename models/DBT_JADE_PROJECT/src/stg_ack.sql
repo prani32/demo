5e8f9eb87bfbb2245568b2cea919ba686db7ef5a
praneetha.katragadda@jadeglobal.com <praneetha.katragadda@jadeglobal.com>
2023-01-10 15:23:55 +0000

    commit

--- models/DBT_JADE_PROJECT/src/stg_ack.sql
+++ models/DBT_JADE_PROJECT/src/stg_ack.sql
@@ -1,11 +1,5 @@
 with source_data AS
 select * from {{ source('shipping_data', 'PO_ACK_855') }}
-),
-ack_data as
-(
-  SELECT ACKNOWLEDGEMENT_RESPONSE,
-  SUPPLIER_ORDER_NUMBER AS SHIPMENT_NUMBER
-  FROM DBT_DB.DBT_SCHEMA.PO_ACK_855
 )
 
 select * from source_data
--- models/DBT_JADE_PROJECT/src/stg_adv.sql
+++ models/DBT_JADE_PROJECT/src/stg_adv.sql
@@ -1,14 +1,5 @@
 with source_data AS
 select * from {{ source('shipping_data', 'ADJ_ADV_947') }}
-),
-ack_data as
-(
-select
-        SHIPMENT_NUMBER,
-        ITEM,
-        VENDOR_NAME,
-        TRANSFER_ORGANIZATION_NAME
-        from "DBT_DB"."DBT_SCHEMA"."ADJ_ADV_947"
 )
 
 select * from source_data
