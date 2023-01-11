cd60ff1e38f8a1834baf80c3664325cdd16eaba7
praneetha.katragadda@jadeglobal.com <praneetha.katragadda@jadeglobal.com>
2023-01-11 14:41:35 +0000

    commit

--- models/DBT_JADE_PROJECT/src/test_incr2.sql
+++ models/DBT_JADE_PROJECT/src/test_incr2.sql
@@ -1,12 +1,12 @@
 {{
     config(
         materialized='incremental',
-        unique_key='EXPECTED_RECEIPT_DATE'
+        unique_key='SHIPMENT_NUMBER'
     )
 }}
 select * from {{ref('stg_receipt')}}
 {% if is_incremental() %}
 -- this filter will only be applied on an incremental run
-  where EXPECTED_RECEIPT_DATE >= (select max(EXPECTED_RECEIPT_DATE) from {{ this }})
+  where SHIPMENT_NUMBER >= (select max(SHIPMENT_NUMBER) from {{ this }})
 {% endif %}
 group by 1
