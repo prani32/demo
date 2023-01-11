6f6fa8fa55aeceff32b095441ae86113a1fcac79
praneetha.katragadda@jadeglobal.com <praneetha.katragadda@jadeglobal.com>
2023-01-11 14:38:55 +0000

    commit

--- models/DBT_JADE_PROJECT/src/test_incr2.sql
+++ models/DBT_JADE_PROJECT/src/test_incr2.sql
@@ -4,13 +4,9 @@
         unique_key='EXPECTED_RECEIPT_DATE'
     )
 }}
-with incr_data1 as
-(
 select * from {{ref('stg_receipt')}}
 {% if is_incremental() %}
 -- this filter will only be applied on an incremental run
   where EXPECTED_RECEIPT_DATE >= (select max(EXPECTED_RECEIPT_DATE) from {{ this }})
 {% endif %}
 group by 1
-)
-select * from incr_data1
