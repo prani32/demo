316d9e11c45e985d9ce734089ea0e64e89275c18
praneetha.katragadda@jadeglobal.com <praneetha.katragadda@jadeglobal.com>
2023-01-12 08:46:52 +0000

    commit

--- models/DBT_JADE_PROJECT/src/test_append.sql
+++ models/DBT_JADE_PROJECT/src/test_append.sql
@@ -2,7 +2,7 @@
     materialized='incremental',
     incremental_strategy='append'
 )}}
-select * from {{ref('stg_receipt')}}
+select * from {{ref('stg_customerdata')}}
 {% if is_incremental() %}
 -- this filter will only be applied on an incremental run
   where SHIPPED_DATE >= (select max(SHIPPED_DATE) from {{ this }})
