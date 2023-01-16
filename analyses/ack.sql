f57ab73f95b8298eb8e7c1463b162c20c16957d5
praneetha.katragadda@jadeglobal.com <praneetha.katragadda@jadeglobal.com>
2023-01-16 07:51:42 +0000

    commit

--- analyses/ack.sql
+++ analyses/ack.sql
@@ -1,13 +1,11 @@
 WITH ack_reviews AS (
  SELECT * FROM {{ ref('stg_ack') }}
 )
-SELECT
- ORDER_NUMBER,
- SUPPLIER_ORDER_NUMBER,
- COUNT(*) as ack
-FROM
-ack_reviews
-GROUP BY
- RESPONSE
-ORDER BY
- SOLD_TO_LE_NAME
+select
+    SOLD_TO_LE_NAME,
+    sum(case when ACKNOWLEDGEMENT_NOTE = 'RE'   then 1 else 0 end) reject,
+    sum(case when ACKNOWLEDGEMENT_NOTE= 'AC' then 1 else 0 end) Accept,
+    count(*) total
+from  DBT_DB.DBT_SCHEMA.PO_ACK_855
+group by SOLD_TO_LE_NAME
+order by SOLD_TO_LE_NAME
