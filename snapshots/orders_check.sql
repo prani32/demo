b495cff3cca4d3b004a8cad9e8c837e9c1f47d67
praneetha.katragadda@jadeglobal.com <praneetha.katragadda@jadeglobal.com>
2023-01-18 06:28:48 +0000

    commit

--- snapshots/orders_check.sql
+++ snapshots/orders_check.sql
@@ -5,7 +5,8 @@
           target_schema='snapshot',
           strategy='check',
           unique_key='id',
-          check_cols = 'all'
+          check_cols = 'all',
+          invalidate_hard_deletes=True
         )
     }}
     select * from {{ source('shipping_data', 'orders') }}
